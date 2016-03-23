255c54f4901b94f6c06a73512aebe39eebb5fa20
Ankit Sharma <ankitkumarsharma939@gmail.com>
2016-03-23 18:29:57 +0000

    new

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -12,11 +12,9 @@
 module.exports = function(grunt) {
 
   grunt.config.set('uglify', {
-    uglify: {
-    options: {
-      mangle: false
-    }
-    },
+		options: {
+			mangle: false
+		},
     dist: {
       src: ['.tmp/public/concat/production.js'],
       dest: '.tmp/public/min/production.min.js'
