27628cc7b933e8843496ec8cafd2c6e7b6d30c32
Ankit Sharma <ankitkumarsharma939@gmail.com>
2016-04-14 15:02:10 +0000

    sfaf

--- .tmp/public/js/public/homepage/HomeCtrl.js
+++
@@ -1,5 +0,0 @@
-/*global angular*/
-angular.module('HomeMod').controller('HomeCtrl',['$scope', '$http','toastr', function($scope, $http, toastr){
-	$scope.data= true;
-	$scope.foundation = true;
-}])
