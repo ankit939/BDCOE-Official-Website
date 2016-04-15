af0da4615a6b74f77ca895bd13bc0babb479f520
Ankit Sharma <ankitkumarsharma939@gmail.com>
2016-04-15 14:33:01 +0000

    fasfa

---
+++ .tmp/public/js/public/homepage/HomeCtrl.js
@@ -0,0 +1,5 @@
+/*global angular*/
+angular.module('HomeMod').controller('HomeCtrl',['$scope', '$http','toastr', function($scope, $http, toastr){
+	$scope.data= true;
+	$scope.foundation = true;
+}])
---
+++ .tmp/public/js/public/login/LoginCtrl.js
@@ -0,0 +1,25 @@
+/*global angular*/
+angular.module('LoginMod').controller('LoginCtrl',['$scope', '$http','toastr', function($scope, $http, toastr){
+	
+	$scope.runLogin = function(){
+		$http.put('/login',{
+			email: $scope.email,
+			password: $scope.password
+		}).then(function onSuccess(){
+			window.location = '/dashboard';
+		}).catch(function OnError(err){
+			if(err.status == 400 || 404){
+				toastr.error('Invalid Credentials','Error', {
+					closeButton:true
+				})
+				$scope.email = "";
+				$scope.password = "";
+				return;	
+			}
+			toastr.error('An error has occured, please try again later', 'Error', {
+				closeButton:true
+			});
+			return;
+		})
+	}
+}])
